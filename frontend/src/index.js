c59e4cb44c59d39650755f77093ff9644d0b9af1
BlueBird6 <102379000+BlueBird6@users.noreply.github.com>
2023-05-24 04:31:18 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
 import './index.css';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:4000/graphql',
+  uri: "http://127.0.0.1:4000/graphql",
+  cache: new InMemoryCache()
 });
+
 
 ReactDOM.render(
   <ApolloProvider client={client}>
